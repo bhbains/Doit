9a208e73370666379122296214a6cf6c847f8485
bhb <bhbains@gmail.com>
2017-07-19 06:51:24 -0700

    Tasks and important

---
+++ Doit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Brian on 7/19/17.
+//  Copyright © 2017 Brian. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
